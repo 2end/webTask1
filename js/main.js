33608854cfb182ea5e85a4d12c1a0b276e1feac1
Roman Myshchyshyn <roman1999.myshchyshyn@gmail.com>
2019-02-24 22:53:15 +0200

    add header title

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
     const layerOne = $('.header-layer-1');
-	const layerTwo = $('.header-layer-2');
+    const layerTwo = $('.header-layer-2');
+    const title = $('.header-title')
 
 	$('header').mousemove(function(e) {
 		const pageX = e.clientX;
 		const pageY = e.clientY;		
   	    layerOne.css({ transform: 'translateX(' + pageX/100 + '%) translateY(' + pageY/100 + '%)'});  	
-  	    layerTwo.css({ transform: 'translateX(' + pageX/150 + '%) translateY(' + pageY/250 + '%)'});		
+        layerTwo.css({ transform: 'translateX(' + pageX/150 + '%) translateY(' + pageY/250 + '%)'});
+        title.css({ transform: 'translateX(' + pageX/150 + '%) translateY(' + pageY/250 + '%)'});		
 	});
  });
