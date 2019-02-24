9d8c0fcc66816cdda71de96fc2a47a20370dae22
Roman Myshchyshyn <roman1999.myshchyshyn@gmail.com>
2019-02-24 20:52:46 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    const layerOne = $('.header-layer-1');
+	const layerTwo = $('.header-layer-2');
+
+	$('header').mousemove(function(e) {
+		const pageX = e.clientX;
+		const pageY = e.clientY;		
+  	    layerOne.css({ transform: 'translateX(' + pageX/100 + '%) translateY(' + pageY/100 + '%)'});  	
+  	    layerTwo.css({ transform: 'translateX(' + pageX/150 + '%) translateY(' + pageY/250 + '%)'});		
+	});
+ });
