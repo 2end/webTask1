d021347aa4c5a6db94ff8a8620714515c2752480
Roman Myshchyshyn <roman1999.myshchyshyn@gmail.com>
2019-02-25 22:43:13 +0200

    add section about and part of section projects

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     const layerOne = $('.header-layer-1');
     const layerTwo = $('.header-layer-2');
-    const title = $('.header-title')
+    const title = $('.header-title');
 
 	$('header').mousemove(function(e) {
 		const pageX = e.clientX;
@@ -9,5 +9,17 @@ $(document).ready(function(){
   	    layerOne.css({ transform: 'translateX(' + pageX/100 + '%) translateY(' + pageY/100 + '%)'});  	
         layerTwo.css({ transform: 'translateX(' + pageX/150 + '%) translateY(' + pageY/250 + '%)'});
         title.css({ transform: 'translateX(' + pageX/150 + '%) translateY(' + pageY/250 + '%)'});		
-	});
+    });
+    
+    $(window).scroll(function() {
+        var st = $(this).scrollTop();
+
+        title.css({ transform: 'translateX(0%) translateY(' + -st / 3 + '%)'});
+        layerOne.css({ transform: 'translateX(0%) translateY(' + -st / 40 + '%)'});
+        layerTwo.css({ transform: 'translateX(0%) translateY(' + -st / 50 + '%)'});
+    });
+
+    AOS.init({
+        easing: 'ease-in-out-sine'
+      });
  });
